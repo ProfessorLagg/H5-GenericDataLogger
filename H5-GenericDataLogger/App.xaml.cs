329e0f6f6e1aa44abea1b731ab6b6c3669959d13
ProfessorLagg <futurecake2999@gmail.com>
2025-06-08 21:50:28 +0200

    Main Page Cards

--- H5-GenericDataLogger/App.xaml.cs
+++ H5-GenericDataLogger/App.xaml.cs
@@ -1,9 +1,11 @@
 ﻿namespace H5_GenericDataLogger {
-    public partial class App : Application {
-        public App() { InitializeComponent(); }
+	public partial class App : Application {
+		public App() {
+			this.InitializeComponent();
+		}
 
-        protected override Window CreateWindow(IActivationState? activationState) {
-            return new Window(new AppShell());
-        }
-    }
+		protected override Window CreateWindow(IActivationState? activationState) {
+			return new Window(new AppShell());
+		}
+	}
 }
--- H5-GenericDataLogger/MainPage.xaml.cs
+++ H5-GenericDataLogger/MainPage.xaml.cs
@@ -1,12 +1,5 @@
 ﻿namespace H5_GenericDataLogger {
     public partial class MainPage : ContentPage {
-        int count = 0;
-
         public MainPage() { InitializeComponent(); }
-
-        private void OnCounterClicked(object? sender, EventArgs e) {
-            CounterBtn.Text = $"SQLite connection string: " + GlobalEnvironment.Connector.ConnectionString;
-            SemanticScreenReader.Announce(CounterBtn.Text);
-        }
     }
 }
