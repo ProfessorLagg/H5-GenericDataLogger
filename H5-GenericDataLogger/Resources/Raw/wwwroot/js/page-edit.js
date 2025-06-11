cbf790159ed0c7aacebd20a87af3608097b25c7f
ProfessorLagg <futurecake2999@gmail.com>
2025-06-11 19:33:04 +0200

    Even more UI

--- H5-GenericDataLogger/Resources/Raw/wwwroot/js/page-edit.js
+++ H5-GenericDataLogger/Resources/Raw/wwwroot/js/page-edit.js
@@ -1,11 +1,55 @@
 const queryParams = new URLSearchParams(window.location.search);
-const LogId = queryParams.get('logid') ?? -1
+const LogId = parseInt(queryParams.get('logid') ?? -1);
+
+const CreateNew = LogId == -1;
+
+const edit_title_input = document.getElementById('edit-title-input');
+const edit_fields_section = document.getElementById('edit-fields-section');
+
+const original_values = {
+    title: "",
+    fields: []
+}
+var current_values;
 
 async function initPageEdit() {
     WriteDebug(arguments.callee.name);
     await updateLogInfo();
 
-    
+    if (!CreateNew) {
+        // TODO handle editing existing logs
+        alert("Editing existing logs is not implemented yet")
+        navigation.navigate("/home.html")
+    }
+
+    current_values = structuredClone(original_values);
+    await validateTitle();
+}
+
+function values_changed() {
+    return !(JSON.stringify(original_values) === JSON.stringify(current_values));
+}
+
+async function save() {
+    if (!values_changed()) return;
+}
+
+async function validateTitle() {
+    edit_title_input.value = edit_title_input.value.trim()
+    let isValid =
+        edit_title_input.value.length > 0
+        && edit_title_input.value.length < 256
+        && isAlphaNumericASCII(edit_title_input.value);
+
+    console.log("isValid:", isValid)
+    if (isValid) {
+        edit_title_input.setAttribute('valid', "");
+        edit_title_input.removeAttribute('invalid');
+    } else {
+        edit_title_input.setAttribute('invalid', "");
+        edit_title_input.removeAttribute('valid');
+
+    }
 }
 
 initPageEdit();
